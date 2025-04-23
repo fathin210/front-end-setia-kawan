fc84b2227af572c3e0d2fdf386352fda456f24ec
Fathin <fathin210@gmail.com>
2025-04-23 10:44:32 +0700

    feature : adjust stale time useListQueue

--- src/hooks/useListQueue.js
+++ src/hooks/useListQueue.js
@@ -7,7 +7,7 @@ const useListQueue = (date, search, status = "x") => {
     queryFn: () => {
       return fetcher(`${import.meta.env.VITE_API_BASE_URL}/daftar?date=${date}&search=${search}&status=${status ?? ''}`)
     },
-    staleTime: 1000 * 60 * 2, // Cache selama 5 menit
+    staleTime: 1000 * 20, // Cache selama 20 detik
   });
 };
 
